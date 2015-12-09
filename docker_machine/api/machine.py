5668cc8398d030ce60d9e3e6e691a7cd8fb6edba
Jonathan Rowlands <jonrowlands83@gmail.com>
2015-12-08 22:55:32 -0500

    Creates generic docker_machine method. Creates Machine class and fills it out using docker-machine inspect and status. Defaults driver to none. Adds none driver test

--- docker_machine/api/machine.py
+++ docker_machine/api/machine.py
@@ -1,35 +1,65 @@
 from subprocess import Popen, PIPE
 import re
+import json
 from .. import errors
 
-class MachineApiMixin(object):
-    def machines(self, quiet=False, all=False, trunc=False, latest=False,
-               since=None, before=None, limit=-1, size=False,
-               filters=None):
 
-        output = Popen(['docker-machine', 'ls'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+class Machine(object):
+    def __init__(self, name, config_version, driver_name, driver, host_options, raw_driver, machine_status):
+        self.name = name
+        self.config_version = config_version
+        self.driver_name = driver_name
+        self.driver = driver
+        self.host_options = host_options
+        self.raw_driver = raw_driver
+        self.machine_status = machine_status
+
+    @staticmethod
+    def create_from_inspect(json, machine_status):
+        name = json['Name']
+        raw_driver = json['RawDriver']
+        config_version = json['ConfigVersion']
+        driver_name = json['DriverName']
+        driver = json['Driver']
+        host_options = json['HostOptions']
+
+        return Machine(name, config_version, driver_name, driver, host_options, raw_driver, machine_status)
+
+
+class MachineApiMixin(object):
+
+    @staticmethod
+    def cmd(command):
+        output = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
         out, err = output.communicate()
 
-        # rc = output.returncode
-        # if rc != 0:
-        #     # raise errors.AnsibleError('Error with executing docker-machine env')
-        #     # raise Error
-        #     raise Exception('YO')
+        if err:
+            raise errors.DockerMachineException(err)
+        return out
 
-        lines = out.splitlines()
-        del lines[0]
+    def machine_names(self):
+        out = self.cmd(['docker-machine', 'ls', '-q'])
+        machine_names = out.splitlines()
+        return machine_names
 
-        machines = list(map((lambda x: re.split('\s+', x)), lines))
-        machines = list(
-            map((lambda x: {'name': x[0], 'active': x[1], 'driver': x[2], 'state': x[3], 'url': x[4], 'swarm': x[5]}),
-                machines))
+    def machine_inspect(self, machine_name):
+        out = self.cmd(['docker-machine', 'inspect', machine_name])
+        return json.loads(out)
 
-        # return err
+    def machine(self, machine_name):
+        machine_status = self.machine_status(machine_name)
+        inspect = self.machine_inspect(machine_name)
+        return Machine.create_from_inspect(inspect, machine_status)
+
+    def machines(self):
+        machines = []
+        for machine_name in self.machine_names():
+            machines.append(self.machine(machine_name))
+
         return machines
 
     def machine_status(self, name):
-        output = Popen(['docker-machine', 'status', name], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        out, err = output.communicate()
+        out = self.cmd(['docker-machine', 'status', name])
 
         s = out.rstrip('\n').lower()
         # if s not in ['running']:
@@ -40,22 +70,13 @@ class MachineApiMixin(object):
     def create_machine(self, name, driver_config=None):
         command = ['docker-machine', 'create']
 
-        # if driver_config is None:
-        #     command.extend(['--driver', 'none'])
-        # else:
-        #     for key, value in driver_config.iteritems():
-        #         command.extend([key, value])
-
-        # TODO: make args a list
-        command.extend(driver_config.args())
+        if driver_config is None:
+            command.extend(['--driver', 'none', '--url', 'localhost'])
+        else:
+            command.extend(driver_config.args())
 
         command.append(name)
-
-        output = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        out, err = output.communicate()
-        if err is not None:
-            raise errors.DockerMachineException(err)
-        return out
+        self.cmd(command)
 
     def remove_machine(self, name, force=False):
         command = ['docker-machine', 'rm']
@@ -65,6 +86,5 @@ class MachineApiMixin(object):
 
         command.append(name)
 
-        output = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        out, err = output.communicate()
-        return out
+        return self.cmd(command)
+
--- tests/integration/machine_test.py
+++ tests/integration/machine_test.py
@@ -1,43 +1,21 @@
 from . import api_test
 
-class ListMachinesTest(api_test.BaseTestCase):
-    def test_list_machines(self):
-        # self.assertFalse(self.client.machines(all=True))
 
-        return True
+class NoDriverMachineTest(api_test.BaseTestCase):
+    def setUp(self):
+        super(NoDriverMachineTest, self).setUp()
+        self.machine_name = 'testNoDriverMachine'
+        self.client.create_machine(self.machine_name)
 
-        # [{'name': 'testMachine', 'url': 'tcp://159.203.118.136:2376', 'driver': 'digitalocean', 'swarm': '', 'state': 'Running', 'active': '-'}] is not false
+    def test_create_machine_no_driver(self):
+        # No Driver has empty status
+        self.assertEqual(self.client.machine_status(self.machine_name), '')
 
-        # self.assertEqual(True, True)
-        # res0 = self.client.containers(all=True)
-        # size = len(res0)
-        # res1 = self.client.create_container(BUSYBOX, 'true')
-        # self.assertIn('Id', res1)
-        # self.client.start(res1['Id'])
-        # self.tmp_containers.append(res1['Id'])
-        # res2 = self.client.containers(all=True)
-        # self.assertEqual(size + 1, len(res2))
-        # retrieved = [x for x in res2 if x['Id'].startswith(res1['Id'])]
-        # self.assertEqual(len(retrieved), 1)
-        # retrieved = retrieved[0]
-        # self.assertIn('Command', retrieved)
-        # self.assertEqual(retrieved['Command'], six.text_type('true'))
-        # self.assertIn('Image', retrieved)
-        # self.assertRegex(retrieved['Image'], r'busybox:.*')
-        # self.assertIn('Status', retrieved)
+        machine = self.client.machine(self.machine_name)
+        self.assertEqual(machine.name, self.machine_name)
+        self.assertEqual(machine.driver_name, 'none')
+        self.assertEqual(machine.machine_status, '')
 
+    def tearDown(self):
+        self.client.remove_machine(self.machine_name, force=True)
 
-class MachineStatusTest(api_test.BaseTestCase):
-    def test_machine_status(self):
-        self.assertEquals(self.client.machine_status('testMachine'), 'running')
-
-class CreateMachinesTest(api_test.BaseTestCase):
-    def test_create_machine(self):
-        driver_config = {'--driver': 'digitalocean', '--digitalocean-access-token': ''}
-        # self.assertFalse(self.client.create_machine('testMachine', driver_config))
-
-class RemoveMachinesTest(api_test.BaseTestCase):
-    def test_remove_machine(self):
-        return True
-        # self.assertFalse(self.client.remove_machine('testMachine'))
-        # Successfully removed testMachine\n'
