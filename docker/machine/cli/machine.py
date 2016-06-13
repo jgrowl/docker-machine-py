954bc39be1aabbb5fc8bfa47e223cfc663eb8935
Jonathan Rowlands <jonrowlands83@gmail.com>
2016-06-13 18:00:40 -0500

    Provides a way to pass a client directly to a machine, sets as a member, and uses throughout. If none is provided (default behavior) the module client variable will be used and saved as a member. This should prevent unexpected behavior if the module variable gets changed after a machine is created.

--- docker/machine/cli/machine.py
+++ docker/machine/cli/machine.py
@@ -5,12 +5,12 @@ import inspect
 class Machine(object):
 
     @classmethod
-    def client(cls):
+    def default_client(cls):
         return docker.machine.CLIENT
 
     @classmethod
     def docker_machine_version(cls):
-        return cls.client().version().val()
+        return cls.default_client().version().val()
 
     @classmethod
     def all_docker_machines(cls):
@@ -19,19 +19,20 @@ class Machine(object):
 
     @classmethod
     def active_docker_machine_names(cls):
-        return cls.client().active().val()
+        return cls.default_client().active().val()
 
     @classmethod
     def active_docker_machines(cls):
-        machine_names = cls.client().active().val()
+        machine_names = cls.default_client().active().val()
         return map(cls, machine_names) if machine_names is not None else list()
 
     @classmethod
     def all_docker_machine_names(cls):
-        return cls.client().ls(quiet=True).val()
+        return cls.default_client().ls(quiet=True).val()
 
-    def __init__(self, name):
+    def __init__(self, name, client=None):
         self.name = name
+        self.client = self.default_client() if client is None else client
 
         if self.exists():
             self._initialize()
@@ -39,18 +40,18 @@ class Machine(object):
             self.inspection = None
 
     def __getattr__(self, name):
-        if hasattr(self.client(), name):
+        if hasattr(self.client, name):
             def wrapper(*args, **kw):
-                fun_name = getattr(self.client(), name)
+                fun_name = getattr(self.client, name)
                 argspec = inspect.getargspec(fun_name)
 
                 client_output = None
                 if 'machine_name' in argspec.args:
                     args = (self.name,) + args
-                    client_output = getattr(self.client(), name)(*args, **kw)
+                    client_output = getattr(self.client, name)(*args, **kw)
                 elif argspec.varargs == 'machine_names':
                     args = args + (self.name,)
-                    client_output = getattr(self.client(), name)(*args, **kw)
+                    client_output = getattr(self.client, name)(*args, **kw)
 
                 after_callback = '_after_{}'.format(name)
                 if hasattr(self, after_callback):
@@ -68,10 +69,10 @@ class Machine(object):
         return self.name == other.name
 
     def exists(self):
-        return len(self.client().ls(quiet=True, filters='name={}'.format(self.name)).formatted) != 0
+        return len(self.client.ls(quiet=True, filters='name={}'.format(self.name)).formatted) != 0
 
     def _after_create(self):
         self._initialize()
 
     def _initialize(self):
-        self.inspection = self.client().inspect(self.name, snake_case=True, named_tuple=True).formatted
+        self.inspection = self.client.inspect(self.name, snake_case=True, named_tuple=True).formatted
