f214d0ddac4bb3fd1fb5c373ad46e397dbb364e7
Jonathan Rowlands <jonrowlands83@gmail.com>
2016-06-14 13:45:59 -0500

    Removes unecessary imports from module's init.

--- docker/machine/__init__.py
+++ docker/machine/__init__.py
@@ -3,6 +3,5 @@ from .version import version
 __version__ = version
 __title__ = 'docker-machine-py'
 
-from .cli.machine import Machine
-from .cli.client import Client, Status, Filter
+from .cli.client import Client
 CLIENT = Client()
